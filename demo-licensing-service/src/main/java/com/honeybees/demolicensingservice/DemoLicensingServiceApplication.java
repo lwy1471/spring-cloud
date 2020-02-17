ebb2b7c37b3f779a6a3e7066fd4e1139a5c618f1
이동훈 <honeybees@sk.com>
2020-02-17 22:55:51 +0900

    re-organized

---
+++ demo-licensing-service/src/main/java/com/honeybees/demolicensingservice/DemoLicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.honeybees.demolicensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLicensingServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoLicensingServiceApplication.class, args);
+  }
+
+}
---
+++ demo-licensing-service/src/test/java/com/honeybees/demolicensingservice/DemoLicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honeybees.demolicensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLicensingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
